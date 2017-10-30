ac8b447d2d8e99cb30a5aeb8764431595d7a50cc
Lukas Brandt <>
2017-10-30 14:34:33 +0100

    corpus save to file

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 "use strict";
 
+const fs = require("fs");
 const wiki = require("wikijs").default;
 const pages = require("./pages");
 
 const content = Promise.all(pages.map(async ({ en }) => {
 	const corpusPage = await wiki().page(en);
 
-	return [en, pipeline(await corpusPage.summary())];
-})).then(result => new Map(result));
+	console.log(`Cleaning article ${en}`);
 
-content.then(console.log);
+	return [en, pipeline(await corpusPage.summary())];
+})).then(result => result.reduce((map, [k, v]) => {
+	map[k] = v;
+
+	return map;
+}, {}));
+
+content.then(content => fs.writeFile("./data/corpus.json", JSON.stringify(content)));
 
 function pipeline (content) {
 	return tokenize(removeParenthesis(content));
