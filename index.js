58048b08ee8be52ad646a680a104bcab67be73d5
Lukas Brandt <>
2017-11-15 15:47:16 +0100

    removed parethesis parser

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const content = Promise.all(pages.map(async ({ en }) => {
 
 	console.log(`Cleaning article ${en}`);
 
-	return [en, removeParenthesis(await corpusPage.summary())];
+	return [en, await corpusPage.summary()];
 })).then(result => result.reduce((map, [k, v]) => {
 	map[k] = v;
 
@@ -17,37 +17,3 @@ const content = Promise.all(pages.map(async ({ en }) => {
 }, {}));
 
 content.then(content => fs.writeFile("./data/corpus.json", JSON.stringify(content)));
-
-function removeParenthesis (content) {
-	const stack = [];
-	let result = "";
-
-	for(let i = 0; i < content.length; i++) {
-		switch (content[i]) {
-		case "(":
-			stack.unshift("(");
-			break;
-		case "[":
-			stack.unshift("[");
-			break;
-		case ")":
-			while(stack[0] !== "(" && stack.length > 0) {
-				stack.shift();
-			}
-			stack.shift();
-			break;
-		case "]":
-			while(stack[0] !== "]" && stack.length > 0) {
-				stack.shift();
-			}
-			stack.shift();
-			break;
-		default:
-			if(stack.length === 0) {
-				result += content[i];
-			}
-		}
-	}
-
-	return result;
-}
